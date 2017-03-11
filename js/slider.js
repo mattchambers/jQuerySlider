e0e6ba76c66493200da274813e4098bb813295ce
Matt <matt.keizergroup@gmail.com>
2017-03-10 20:03:22 -0500

    Cleaned up javascript

--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,26 @@
 $(function(){
-	//hide images that are not active
-	$('.content:not(.active)').hide();
-
-	//cache DOM elements
 	var $nextBtn = $('.slider .buttons .nextBtn');
 	var $prevBtn = $('.slider .buttons .prevBtn');
 
-	$nextBtn.on('click', function(){
-
+	function next(){
 		var $slider = $(this).closest('.slider');
+		var $activeContent = $slider.find('.active');
+		
 
-		$slider.find('.content.active').next().addClass('active');
-		$slider.find('.content.active').prev().removeClass('active');
+		$activeContent.next().addClass('active').prev().removeClass('active');
 
-		$slider.find('.content:not(.active)').hide();
-		$slider.find('.active').show();
+		
+	}
 
-	})
-
-	$prevBtn.on('click', function(){
+	function prev(){
 		var $slider = $(this).closest('.slider');
+		var $activeContent = $slider.find('.active');
+		
 
-		$slider.find('.content.active').prev().addClass('active');
-		$slider.find('.content.active').next().removeClass('active');
+		$activeContent.prev().addClass('active').next().removeClass('active');
+	}
 
-		$slider.find('.content:not(.active)').hide();
-		$slider.find('.active').show();
-	})
+	$nextBtn.on('click', next);
+	$prevBtn.on('click', prev);
+
 });
